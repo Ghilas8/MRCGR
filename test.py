8dd113b04c87f5fe7b6d2e3987499fdd3126077d
rvila94 <96233869+rvila94@users.noreply.github.com>
2023-01-18 14:50:01 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from robot import Robot
 #CREATION ROBOT (position 0,0 et regarde vers le haut)
 robot1 = Robot(0,0,1)
 
+
 #AVANCER
 print("position avant" ,robot1.getPos())
 for i in range(0,10):
@@ -12,14 +13,13 @@ for i in range(0,10):
 
 
 #RECULER
-
 print("position avant" ,robot1.getPos())
 for i in range(0,10):
     robot1.reculer()
     print("position apres" ,i,robot1.getPos())
-    
-#TOURNER
+   
 
+#TOURNER
 print("direction de base: ",robot1.getDirr())
 robot1.tourner("d")
 print("apres tourner a doite: ",robot1.getDirr())
