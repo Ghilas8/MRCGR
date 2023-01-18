b4e712b50dc22b584cefa0bfbb81c3f9f9f033d3
rvila94 <96233869+rvila94@users.noreply.github.com>
2023-01-18 14:46:50 +0100

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,17 +1,17 @@
 class Robot:
 	def __init__(self,posx, posy, dirr):
-		self.posx = posx                                    #position du robot sur l'axe des abscisse 
-		self.posy = posy                                    #position du robot sur l'axe des abscisse 
-		self.dirr  = dirr                                    #direction du robot (1 pour haut, 2 pour droite, 3 pour bas, 4 pour gauche)
+		self.posx = posx					#position du robot sur l'axe des abscisse 
+		self.posy = posy					#position du robot sur l'axe des abscisse 
+		self.dirr  = dirr					#direction du robot (1 pour haut, 2 pour droite, 3 pour bas, 4 pour gauche)
 	
-	def getPos(self):                                       #retourne la position
+	def getPos(self):						#retourne la position
 		return self.posx, self.posy
 		
-	def getDirr(self):                                       #retourne la direction
+	def getDirr(self):						#retourne la direction
 		return self.dirr
 		
-	def avancer(self):                                      #avance le robot de 1 (en fonction de sa direction) en prenant en compte que le point en bas a gauche est (0,0)
-
+	def avancer(self):                                      	#avance le robot de 1 (en fonction de sa direction) 
+									#en prenant en compte que le point en bas a gauche est (0,0)
 		if(self.dirr==1):
 			self.posy +=1
 		elif(self.dirr==2):
@@ -23,7 +23,7 @@ class Robot:
 		else:
 			self.posx -=1
 	
-	def reculer(self):                                      #avance le robot de 1 (en fonction de sa direction) en prenant en compte que le point en bas a gauche est (0,0)
+	def reculer(self):						#recule le robot (similairement a avancer)
 
 		if(self.dirr==1):
 			self.posy -=1
@@ -37,14 +37,25 @@ class Robot:
 		else:
 			self.posx +=1
 	
-	def tourner(self, direction):                           #tourne le robot selon une direction ("g" pour gauche, "d" pour droite)
-		pass
+	def tourner(self, direction):					#tourne le robot selon une direction
+        	if (direction == "d"):                                  	#cas "d" pour droite 
+            		self.dirr = (self.dirr%4) + 1
+        	elif (direction == "g"):                                	#cas "g" pour gauche
+            		if (self.dirr == 1): 
+               			self.dirr=4
+            		else:
+                   		self.dirr -= 1
+
+            
+        	else:                                                   	#cas argument non valide
+            		print("Mauvais argument, le robot n'a pas tourne. Les parametres possibles sont: 'd' ou 'g'")
+			
 	
-	def collision(self, tabPoints):                         #verifie si le robot fait une collision avec un objet grace à un tableau de points
-	                                                        #regarde si position du robot et dans le tableau de points
+	def collision(self, tabPoints):					#verifie si le robot fait une collision avec un objet grace à un tableau de points
+	                                                        	#regarde si position du robot est dans le tableau de points
 		pass
 
 
 
-		
+
 	
