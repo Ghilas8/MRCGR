796dd8201d0bf493b771be7a4ec4e25dd5cb06ce
Ghilas8 <79231769+Ghilas8@users.noreply.github.com>
2023-01-24 13:44:23 +0100

    Create Obstacle.py

---
+++ Obstacle.py
@@ -0,0 +1,23 @@
+class Obstacle:
+    def init(self,posx, posy, tailleX, tailleY):
+        self.posx     = posx                    #position du point le plus en bas a gauche du rectangle en abscisse 
+        self.posy     = posy                    #position du point le plus en bas a gauche du rectangle en ordonne
+        self.tailleX = tailleX                    #taille du rectangle en abscisse
+        self.tailleY  = tailleY                    #taille du rectangle en ordonne
+
+    def getPos(self):                            #retourne la position du rectangle (le point le plus en bas a gauche)
+        return (self.posx, self.posy)
+
+    def getDimensions(self):                    #retourne les dimensions du rectangle (tailleX, tailleY)
+        return self.tailleX, self.tailleY
+
+    def listePoints(self):                      #retourne la liste des points qu'occupe l'obstacle (innacessibles aux robots)
+        liste=[]
+        for i in range(self.posx, self.posx + self.tailleX + 1):
+            for j in range(self.posy, self.posy + self.tailleY + 1):
+                liste.append((i,j))
+        return liste
+
+#TEST
+o = Obstacle(0,0,2,4)
+print(o.listePoints())
